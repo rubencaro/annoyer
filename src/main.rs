d0322100d573766b68a8772dbb48e8e0f27568c1
Rubén Caro <ruben.caro.estevez@gmail.com>
2019-07-07 12:36:56 +0200

    Near first working crap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
-fn main() {
-    println!("Hello, world!");
+extern crate futures;
+#[macro_use]
+extern crate clap;
+extern crate hyper;
+extern crate tokio;
+
+use futures::future::{ok, loop_fn, join_all, Future, FutureResult, Loop};
+use std::io::Error;
+
+struct Client {
+    ping_count: u8,
 }
+
+impl Client {
+    fn new() -> Self {
+        Client { ping_count: 0 }
+    }
+
+    fn send_ping(self) -> FutureResult<Self, Error> {
+        ok(Client { ping_count: self.ping_count + 1 })
+    }
+
+    fn receive_pong(self) -> FutureResult<(Self, bool), Error> {
+        let done = self.ping_count >= 5;
+        ok((self, done))
+    }
+}
+
+fn main() {
+    let matches = clap::App::new("Annoyer")
+        .version("0.1.0")
+        .about("Annoying HTML load generator.")
+        .args_from_usage(
+            "-u, --url <string> 'The URL to be called'
+             -c, --concurrency <number> 'Indicates the number of parallel workers'",
+        )
+        .get_matches();
+    let concurrency = parse_u32(&matches, "concurrency", 10);
+
+    let mut parallel = Vec::new();
+    for _i in 0..concurrency {
+        let ping_til_done = loop_fn(Client::new(), |client| {
+            client.send_ping()
+                .and_then(|client| client.receive_pong())
+                .and_then(|(client, done)| {
+                    if done {
+                        Ok(Loop::Break(client))
+                    } else {
+                        Ok(Loop::Continue(client))
+                    }
+                })
+        });
+        parallel.push(ping_til_done);
+    }
+    let work = join_all(parallel).then(|res| {
+            res.unwrap();
+            ok(())
+        });
+
+    tokio::run(work);
+}
+
+fn parse_u32(matches: &clap::ArgMatches<'_>, name: &str, default: u32) -> u32 {
+    value_t!(matches, name, u32).unwrap_or_else(|e| {
+        println!("{}\nUsing default one.", e);
+        default
+    })
+}
+
+// fn parse_url(matches: &clap::ArgMatches<'_>, name: &str) -> hyper::Uri {
+//     matches.value_of(name).unwrap().parse::<hyper::Uri>().unwrap()
+// }
